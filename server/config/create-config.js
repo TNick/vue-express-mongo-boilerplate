4497497bcd5e75eb577267dba4ceef201ac7c480
Nicu Tofan <nicu.tofan@gmail.com>
2017-09-16 00:43:04 +0300

    make sure that this is the script being invocated

--- server/config/create-config.js
+++ server/config/create-config.js
@@ -39,14 +39,17 @@ module.exports = {
 // If the module was invoked directly from command line
 // create config.js from the template right now.
 var args = process.argv.slice(2);
-if (args.length > 1) {
-	if (args[0] === "create") {
-		module.exports.createFromTemplate(args[1]);
-	} else if (args[0] === "touch") {
-		if (!fs.existsSync(args[1])) {
-		 module.exports.createFromTemplate(args[1]);
+if ((process.argv[0].indexOf('create-config') !== -1) ||
+    (process.argv[1].indexOf('create-config') !== -1)) {
+	if (args.length > 1) {
+		if (args[0] === "create") {
+			module.exports.createFromTemplate(args[1]);
+		} else if (args[0] === "touch") {
+			if (!fs.existsSync(args[1])) {
+				module.exports.createFromTemplate(args[1]);
+			}
+		} else {
+			throw `Unknown argument ${args[0]}`;
 		}
-	} else {
-		throw `Unknown argument {args[0]}`;
 	}
 }
