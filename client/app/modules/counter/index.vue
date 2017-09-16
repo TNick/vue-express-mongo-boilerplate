009c22d7090df4552d63e6b4b588656de85f5173
Nicu Tofan <nicu.tofan@gmail.com>
2017-09-16 09:15:31 +0300

    change tabs to spaces (lint)

--- client/app/modules/counter/index.vue
+++ client/app/modules/counter/index.vue
@@ -1,91 +1,91 @@
 <template lang="pug">
-	.container
-		h2.title {{ "Demo" | i18n }}
+ .container
+  h2.title {{ "Demo" | i18n }}
 
-		h3 {{ count }}
-		button.button.success(@click="inc") 
-			span.icon
-				i.fa.fa-arrow-up 
-			span {{ "Increment" | i18n }}
-		br
-		br
-		button.button.warning(@click="dec") 
-			span
-				i.fa.fa-arrow-up  
-			span {{ "Decrement" | i18n }}
+  h3 {{ count }}
+  button.button.success(@click="inc")
+   span.icon
+    i.fa.fa-arrow-up
+   span {{ "Increment" | i18n }}
+  br
+  br
+  button.button.warning(@click="dec")
+   span
+    i.fa.fa-arrow-up
+   span {{ "Decrement" | i18n }}
 
 </template>
 
 <script>
-	import { mapActions, mapGetters } from "vuex";
+ import { mapActions, mapGetters } from "vuex";
 
-	import Service from "../../core/service";
+ import Service from "../../core/service";
 
-	export default {
-		/**
-		 * Computed getters
-		 */
-		 computed: mapGetters("counter", [
-			 "count"
-		 ]),
+ export default {
+  /**
+   * Computed getters
+   */
+   computed: mapGetters("counter", [
+    "count"
+   ]),
 
-		/**
-		 * Page methods
-		 */
-		methods: {
-			/**
-			 * Actions from store
-			 */
-			...mapActions("counter", [
-				"getValue",
-				"increment",
-				"decrement",
-				"changedValue"
-			]),
+  /**
+   * Page methods
+   */
+  methods: {
+   /**
+    * Actions from store
+    */
+   ...mapActions("counter", [
+    "getValue",
+    "increment",
+    "decrement",
+    "changedValue"
+   ]),
 
-			/**
-			 * Increment counter
-			 */
-			inc() {
-				this.increment();
-			},
+   /**
+    * Increment counter
+    */
+   inc() {
+    this.increment();
+   },
 
-			/**
-			 * Decrement counter
-			 */
-			dec() {
-				this.decrement();
-			}
-		},
+   /**
+    * Decrement counter
+    */
+   dec() {
+    this.decrement();
+   }
+  },
 
-		/**
-		 * Socket handlers. Every property is an event handler
-		 */
-		socket: {
+  /**
+   * Socket handlers. Every property is an event handler
+   */
+  socket: {
 
-			prefix: "/counter/",
+   prefix: "/counter/",
 
-			//namespace: "/counter",
+   //namespace: "/counter",
 
-			events: {
-				/**
-				 * Counter value is changed
-				 * @param  {Number} msg Value of counter
-				 */
-				changed(res) {
-					console.log("Counter changed to " + res.data + (res.user ? " by " + res.user.fullName : ""));
-					this.changedValue(res.data);
-				}
-			}
-		},
+   events: {
+    /**
+     * Counter value is changed
+     * @param  {Number} msg Value of counter
+     */
+    changed(res) {
+     console.log("Counter changed to " + res.data + (res.user ? " by " + res.user.fullName : ""));
+     this.changedValue(res.data);
+    }
+   }
+  },
 
-		created() {
-			this.$service = new Service("counter", this); 
-			
-			// Get the latest value of counter
-			this.getValue(); 
-		}
-	};
+  created() {
+   this.$service = new Service("counter", this);
+
+   // Get the latest value of counter
+   this.getValue();
+  }
+ };
 
 </script>
 
